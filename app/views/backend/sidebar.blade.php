937ff9add2bff736e631394b5b8c7866fda47ab8
andreuramos <andreu.ramos.amengual@gmail.com>
2016-03-24 18:46:30 +0100

    dashboard sidebar badges

--- app/views/backend/sidebar.blade.php
+++ app/views/backend/sidebar.blade.php
@@ -6,16 +6,16 @@
             </a>
         </li>
         <li>
-            <a href="{{URL::to('dashboard/beers')}}"><i class="fa fa-beer"></i>&nbsp; Beers</a>
+            <a href="{{URL::to('dashboard/beers')}}"><i class="fa fa-beer"></i>&nbsp; Beers <span class="badge">{{count(Beer::all())}}</span></a>
         </li>
         <li>
-            <a href="{{URL::to('dashboard/brewers')}}"><i class="fa fa-industry"></i>&nbsp; Brewers</a>
+            <a href="{{URL::to('dashboard/brewers')}}"><i class="fa fa-industry"></i>&nbsp; Brewers <span class="badge">{{count(Brewer::all())}}</a>
         </li>
         <li>
-            <a href="{{URL::to('dashboard/localities')}}"><i class="fa fa-globe"></i>&nbsp; Localities</a>
+            <a href="{{URL::to('dashboard/localities')}}"><i class="fa fa-globe"></i>&nbsp; Localities <span class="badge">{{count(Locality::all())}}</a>
         </li>
         <li>
-            <a href="{{URL::to('dashboard/styles')}}"><i class="fa fa-font"></i>&nbsp; Styles</a>
+            <a href="{{URL::to('dashboard/styles')}}"><i class="fa fa-font"></i>&nbsp; Styles <span class="badge">{{count(Style::all())}}</a>
         </li>
         <li>
             <a href="#"><i class="fa fa-user"></i>&nbsp; Account</a>
